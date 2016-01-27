ac39f49e57cb9a19b9cd825805a76a498d624659
382615666 <382615666@qq.com>
2016-01-27 14:20:50 +0800

    管理

--- WebContent/js/admin/public.js
+++ WebContent/js/admin/public.js
@@ -2,5 +2,25 @@ function getHashString(name)
 {
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if(r!=null)return  unescape(r[2]); return null;
+    if(r!=null)return  decodeURI(r[2]); return null;
+}
+function ajaxFileUpload(id) {
+    $.ajaxFileUpload
+    (
+        {
+            url: '/jos/item/uploadDetailImage.action', //用于文件上传的服务器端请求地址
+            secureuri: false, //是否需要安全协议，一般设置为false
+            fileElementId: id, //文件上传域的ID
+            dataType: 'json', //返回值类型 一般设置为json
+            success: function (data, status)  //服务器成功响应处理函数
+            {
+                debugger;
+            },
+            error: function (data, status, e)//服务器响应失败处理函数
+            {
+                alert(e);
+            }
+        }
+    )
+    return false;
 }
