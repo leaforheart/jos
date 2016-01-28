1eca6d240fec56622ef468f37f528a95efa68be8
382615666 <382615666@qq.com>
2016-01-28 13:44:03 +0800

    上传

--- WebContent/js/admin/public.js
+++ WebContent/js/admin/public.js
@@ -4,7 +4,7 @@ function getHashString(name)
     var r = window.location.search.substr(1).match(reg);
     if(r!=null)return  decodeURI(r[2]); return null;
 }
-function ajaxFileUpload(id) {
+function ajaxFileUpload(id,param) {
     $.ajaxFileUpload
     (
         {
@@ -12,9 +12,10 @@ function ajaxFileUpload(id) {
             secureuri: false, //是否需要安全协议，一般设置为false
             fileElementId: id, //文件上传域的ID
             dataType: 'json', //返回值类型 一般设置为json
+            data:param,
             success: function (data, status)  //服务器成功响应处理函数
             {
-                debugger;
+
             },
             error: function (data, status, e)//服务器响应失败处理函数
             {
